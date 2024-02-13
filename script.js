64e93c22185a6b79d8c07097fbf2da0fb7d7fdba
¨jayjay¨ <jhonjhon@gmail.com>
2024-02-13 12:34:49 +0800

    remove mouse hover

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-$(function(){
-    $(".tricky").on({
-        mouseover:function(){
-            $(this).css({
-                left:(Math.random()*90)+"%",
-                top:(Math.random()*90)+"%",
-            });
-        }
+$(function () {
+    // Function to set random position for an element
+    function setRandomPosition(element) {
+        element.css({
+            left: (Math.random() * 90) + "%",
+            top: (Math.random() * 90) + "%",
+        });
+    }
+
+    // Periodically change positions of elements with class "tricky"
+    setInterval(function () {
+        $(".tricky").each(function () {
+            setRandomPosition($(this));
+        });
+    }, 500); // Change position every 3 seconds
+
+    // Hover event for elements with the class "btn-wrap"
+    $(".btn-wrap").hover(function () {
+        $(this).toggleClass('active');
     });
-    $(".btn-wrap").hover(function() {
-           $(this).toggleClass('active');
+
+    // Touchstart event for elements with the class "tricky"
+    $('.tricky').bind('touchstart', function () {
+        setRandomPosition($(this));
     });
 });
-
-$('.tricky').bind('touchstart', function(){
-    $(this).css({
-        left:(Math.random()*90)+"%",
-        top:(Math.random()*90)+"%"
-    });
-})
-
 
 const button = document.getElementById('btnalert');
 button.addEventListener('click', function () {
