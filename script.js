ba4f078681de77fb2f3d910a8a9cdd8a62afed8d
¨jayjay¨ <jhonjhon@gmail.com>
2024-02-13 10:18:37 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+$(function(){
+    $(".tricky").on({
+        mouseover:function(){
+            $(this).css({
+                left:(Math.random()*90)+"%",
+                top:(Math.random()*90)+"%",
+            });
+        }
+    });
+    $(".btn-wrap").hover(function() {
+           $(this).toggleClass('active');
+    });
+});
+
+$('.tricky').bind('touchstart', function(){
+    $(this).css({
+        left:(Math.random()*90)+"%",
+        top:(Math.random()*90)+"%"
+    });
+})
+
+const button = document.getElementById('btnalert')
+button.addEventListener('click', function () {
+alert('I feel dissappointed')
+})
